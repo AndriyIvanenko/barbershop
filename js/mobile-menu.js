419dc1a6be5d6e71711ebe2ce70b99d6b95cfd95
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-02-02 00:04:10 +0200

    220202

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,33 @@
 (() => {
-  const mobileMenuBtn = document.querySelector('[data-mobile-menu-btn]');
-  const mobileMenu = document.querySelector('[data-mobile-menu]');
-  const firstNavItem = document.querySelector('[data-first-nav-link]');
-  
-  mobileMenuBtn.addEventListener('click', toggleMenu);
+	const mobileMenuBtn = document.querySelector("[data-mobile-menu-btn]");
+	const mobileMenu = document.querySelector("[data-mobile-menu]");
+	const firstNavItem = document.querySelector("[data-first-nav-link]");
 
-  function toggleMenu() {
-    const expanded = mobileMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    mobileMenuBtn.classList.toggle('is-open');
-    mobileMenuBtn.setAttribute('aria-expanded', !expanded);
-    mobileMenu.classList.toggle('is-open');
-    document.body.classList.toggle("mobile-menu-open");
+	mobileMenuBtn.addEventListener("click", toggleMenu);
 
-    firstNavItem.focus();
-  }
+	function toggleMenu() {
+		const expanded = mobileMenuBtn.getAttribute("aria-expanded") === "true" || false;
+		mobileMenuBtn.classList.toggle("is-open");
+		mobileMenuBtn.setAttribute("aria-expanded", !expanded);
+		mobileMenu.classList.toggle("is-open");
+		document.body.classList.toggle("mobile-menu-open");
+
+		firstNavItem.focus();
+	}
+
+	// const mediaQuery = window.matchMedia("(max-width: 767px)");
+	// if (window.innerWidth < 768) {
+	let menuLinks = document.getElementsByClassName("menu-link");
+	for (let menuLink of menuLinks) {
+		menuLink.addEventListener("click", closeMenu);
+	}
+
+	function closeMenu() {
+		document.body.classList.remove("mobile-menu-open");
+		mobileMenu.classList.remove("is-open");
+		mobileMenuBtn.classList.remove("is-open");
+		const expanded = mobileMenuBtn.getAttribute("aria-expanded") === "true" || false;
+		mobileMenuBtn.setAttribute("aria-expanded", !expanded);
+	}
+	// }
 })();
